adfe5280e5c439a1066101e3919863d9605b0d3c
sanyamkamat <sanyamkamat@gmail.com>
2014-05-31 01:20:44 +0530

    Just a example of Node running properly

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 /**
  * Created by Sam on 5/29/2014.
  */
+var http = require("http");
+
+http.createServer(function(request,response){
+    response.writeHead(200,{'Content-Type':'text/html'});
+    response.end('<html><body><h1>URL was :'+request.url+'</h1></body></html>');
+}).listen(3000,'localhost');
+
+console.log('Server running at localhost:3000/');
+
